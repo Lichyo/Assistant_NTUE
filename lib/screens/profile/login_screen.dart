254904dbd403be625913993e725b0f0d0710d90c
Lichyo <lichyo003@gmail.com>
2023-03-12 23:25:09 +0800

    Add Progress Animation

--- lib/screens/profile/login_screen.dart
+++ lib/screens/profile/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:assistant/constant.dart';
 import 'package:assistant/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -16,108 +17,117 @@ class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
   String ID = "";
   String password = "";
-  bool isObscure = false;
+  bool isObscure = true;
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Image(
-                image: AssetImage(
-                  'lib/images/NTUE.png',
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Image(
+                  image: AssetImage(
+                    'lib/images/NTUE.png',
+                  ),
+                  height: 90.0,
                 ),
-                height: 90.0,
-              ),
-              SizedBox(
-                width: 20.0,
-              ),
-              Text(
-                'Login',
-                style: kTitleTextStyle,
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 30.0,
-              vertical: 5.0,
-            ),
-            child: TextField(
-              keyboardType: TextInputType.number,
-              decoration:
-                  kTextFieldDecoration.copyWith(labelText: 'Enter your ID'),
-              onChanged: (value) {
-                ID = value;
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 5.0,
-              horizontal: 30.0,
-            ),
-            child: TextField(
-              obscureText: isObscure,
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'Enter your password',
-                suffix: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isObscure = !isObscure;
-                    });
-                  },
-                  child: const Icon(Icons.remove_red_eye_outlined),
+                SizedBox(
+                  width: 20.0,
                 ),
-              ),
-              onChanged: (value) {
-                password = value;
-              },
+                Text(
+                  'Login',
+                  style: kTitleTextStyle,
+                ),
+              ],
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
-            child: ElevatedButton(
-              style: const ButtonStyle(
-                padding: MaterialStatePropertyAll(
-                  EdgeInsets.symmetric(
-                    horizontal: 30.0,
-                    vertical: 10.0,
+            const SizedBox(
+              height: 30.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30.0,
+                vertical: 5.0,
+              ),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration:
+                    kTextFieldDecoration.copyWith(labelText: 'Enter your ID'),
+                onChanged: (value) {
+                  ID = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 5.0,
+                horizontal: 30.0,
+              ),
+              child: TextField(
+                obscureText: isObscure,
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'Enter your password',
+                  suffix: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isObscure = !isObscure;
+                      });
+                    },
+                    child: const Icon(Icons.remove_red_eye_outlined),
                   ),
                 ),
-                backgroundColor: MaterialStatePropertyAll(Colors.lightBlue),
-              ),
-              onPressed: () async {
-                FocusManager.instance.primaryFocus?.unfocus();
-                try {
-                  final user = await _auth.signInWithEmailAndPassword(
-                      email: ID, password: password);
-                  if(user != null) {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Home()));
-                  }
-                  setState(() {});
-                } catch (e) {
-                  print(e);
-                }
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const Home()));
-              },
-              child: const Text(
-                'Submit',
-                style: kButtonTextStyle,
+                onChanged: (value) {
+                  password = value;
+                },
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  padding: MaterialStatePropertyAll(
+                    EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10.0,
+                    ),
+                  ),
+                  backgroundColor: MaterialStatePropertyAll(Colors.lightBlue),
+                ),
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  FocusManager.instance.primaryFocus?.unfocus();
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: ID, password: password);
+                    if(user != null) {
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Home()));
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } catch (e) {
+                    print(e);
+                  }
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => const Home()));
+                },
+                child: const Text(
+                  'Submit',
+                  style: kButtonTextStyle,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/profile/register_screen.dart
+++ lib/screens/profile/register_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:assistant/constant.dart';
 import 'package:assistant/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -19,129 +20,137 @@ class _RegisterScreenState extends State<RegisterScreen> {
   String name = "";
   String email = "";
   bool isObscure = true;
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Image(
-                image: AssetImage(
-                  'lib/images/NTUE.png',
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Image(
+                  image: AssetImage(
+                    'lib/images/NTUE.png',
+                  ),
+                  height: 90.0,
                 ),
-                height: 90.0,
-              ),
-              SizedBox(
-                width: 20.0,
-              ),
-              Text(
-                'Register',
-                style: kTitleTextStyle,
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
-            child: TextField(
-              onChanged: (value) {
-                name = value;
-              },
-              keyboardType: TextInputType.name,
-              decoration:
-                  kTextFieldDecoration.copyWith(labelText: 'Enter your name'),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Text(
+                  'Register',
+                  style: kTitleTextStyle,
+                ),
+              ],
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
-            child: TextField(
-              onChanged: (value) {
-                ID = value;
-              },
-              keyboardType: TextInputType.number,
-              decoration:
-                  kTextFieldDecoration.copyWith(labelText: 'Enter your ID'),
+            const SizedBox(
+              height: 30.0,
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
-            child: TextField(
-              onChanged: (value) {
-                email = value;
-              },
-              keyboardType: TextInputType.emailAddress,
-              decoration:
-                  kTextFieldDecoration.copyWith(labelText: 'Enter your email'),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              child: TextField(
+                onChanged: (value) {
+                  name = value;
+                },
+                keyboardType: TextInputType.name,
+                decoration:
+                    kTextFieldDecoration.copyWith(labelText: 'Enter your name'),
+              ),
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
-            child: TextField(
-              onChanged: (value) {
-                password = value;
-              },
-              keyboardType: TextInputType.text,
-              obscureText: isObscure,
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'Enter your password',
-                suffix: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isObscure = !isObscure;
-                    });
-                  },
-                  child: const Icon(
-                    Icons.remove_red_eye_outlined,
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              child: TextField(
+                onChanged: (value) {
+                  ID = value;
+                },
+                keyboardType: TextInputType.number,
+                decoration:
+                    kTextFieldDecoration.copyWith(labelText: 'Enter your ID'),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              child: TextField(
+                onChanged: (value) {
+                  email = value;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration:
+                    kTextFieldDecoration.copyWith(labelText: 'Enter your email'),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+              child: TextField(
+                onChanged: (value) {
+                  password = value;
+                },
+                keyboardType: TextInputType.text,
+                obscureText: isObscure,
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'Enter your password',
+                  suffix: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isObscure = !isObscure;
+                      });
+                    },
+                    child: const Icon(
+                      Icons.remove_red_eye_outlined,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
-            child: ElevatedButton(
-              style: const ButtonStyle(
-                padding: MaterialStatePropertyAll(
-                  EdgeInsets.symmetric(
-                    horizontal: 30.0,
-                    vertical: 10.0,
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  padding: MaterialStatePropertyAll(
+                    EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10.0,
+                    ),
                   ),
+                  backgroundColor: MaterialStatePropertyAll(Colors.lightBlue),
                 ),
-                backgroundColor: MaterialStatePropertyAll(Colors.lightBlue),
-              ),
-              onPressed: () async {
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(
-                      email: email, password: password);
-                  if (newUser != null) {
-                    setState(() {
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    if (newUser != null) {
                       Navigator.of(context).push(MaterialPageRoute(
                           builder: (context) => const Home()));
+                    }
+                    setState(() {
+                      showSpinner = false;
                     });
+                  } catch (e) {
+                    print(e);
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-              child: const Text(
-                'Submit',
-                style: kButtonTextStyle,
+                },
+                child: const Text(
+                  'Submit',
+                  style: kButtonTextStyle,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
